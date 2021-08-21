5a444826e08b9ff24cd27552cd21b3d330cfbb96
super-rogatory <chukwudi10@gmail.com>
2021-08-21 16:21:33 -0700

    Feature to implement: Fix scrolling feature, ensure client-socket communication

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const PORT = process.env.PORT || 8080
+const app = require('./app')
+const socket = require("socket.io");
+
+const init = async () => {
+  try {
+    // await db.sync()
+    // start listening (and create a 'server' object representing our server)
+    const server = app.listen(PORT, () => console.log(`Mixing it up on port ${PORT}`))
+
+    //socket control
+    const io = socket(server);
+    require("./socket")(io);
+  } catch (ex) {
+    console.log(ex)
+  }
+}
+
+init()
