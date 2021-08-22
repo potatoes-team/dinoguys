eeb9094cdd096ff3319bb5743bcb6da2b0c614e2
super-rogatory <chukwudi10@gmail.com>
2021-08-22 13:27:34 -0700

    Feature: Socket | Client implementations - newPlayerJoined, roomInfo

--- server/socket/index.js
+++ server/socket/index.js
@@ -1,8 +1,25 @@
 const gameRooms = {
-    // store useful info about room keys
+    // store useful info about room keys. metadata -> rooms, players in room with information like socket.id and move state
+    room1: {
+        players: {},
+        playerNum: 0
+    },
+    room2: {
+        players: {},
+        playerNum: 0
+    }
 }
 module.exports = (io) => {
 	io.on('connection', function (socket) {
 		console.log('a user connected');
+        socket.on('joinRoom', (roomKey) => {
+            // console.log(roomKey); -> room1 || room2 for now
+            socket.join(roomKey);
+            socket.to(roomKey).emit("newPlayerJoined", `${socket.id} has joined the mf room`);
+            gameRooms[roomKey].playerNum += 1;
+            gameRooms[roomKey].players[socket.id] = 'moving to the moon';
+            console.log(gameRooms);
+            socket.emit("roomInfo", gameRooms[roomKey]);
+        })
     })
 };
