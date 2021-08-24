83377b2107482521761073da083b1b07c401a077
super-rogatory <chukwudi10@gmail.com>
2021-08-23 23:12:45 -0700

    save because - slow computer

--- src/scenes/LoadingScene.js
+++ src/scenes/LoadingScene.js
@@ -5,17 +5,27 @@ export default class LoadingScene extends Phaser.Scene {
 	constructor() {
 		super('LoadingScene');
 	}
+    init(data) {
+        this.socket = data.socket;
+    }
 	preload() {
         // starts sends a message out, then starts a loop (edge case if the user stalls)
-        this.config = new LoadingSceneConfig(this);
-        this.config.generateRandomHint();
-        this.config.startMessageLoop();
-        
+        this.loadingConfig = new LoadingSceneConfig(this);
+        this.loadingConfig.generateRandomHint();
+        this.loadingConfig.startMessageLoop();
+
         // create loading bar
 		const progressBar = this.add.graphics({
-
+            fillStyle: { color: 0xfff }
         });
-		const progressBox = this.add.graphics();
+		const progressBox = this.add.graphics({
+            lineStyle: { width: 2 }
+        });
+        // want to have the progress box before progress so the bar fills inside the box as assets are loading
+        // 1280. rectangle spans 640. [320] - [640] - [320]
+        // 720. rectange has a height of 50 px. [335] - [50] - [335]
+        progressBox.clear();
+        progressBox.strokeRect(this.scale.width / 2 - 320, 335, this.scale.width / 2, 50);
 		// Load everything that needs to be loaded in
 		// platform & traps
 		this.load.tilemapTiledJSON('tilemap', 'assets/tilemap/J.Test1Map.json');
@@ -36,14 +46,24 @@ export default class LoadingScene extends Phaser.Scene {
 			frameHeight: 18,
 			spacing: 9,
 		});
+        // simulating load
+        for(let i = 0; i < 100; i++) {
+            this.load.spritesheet('loadingdino' + i, 'assets/spriteSheets/dino-blue3.png', {
+                frameWidth: 15,
+                frameHeight: 18,
+                spacing: 9,
+            });
+        }
         // loader event
         this.load.on("progress", (percent) => {
-            console.log(percent);
-            
+            progressBar.clear();
+            progressBox.fillRect(this.scale.width / 2 - 320, 335, this.scale.width / 2, 50);
+            progressBar.fillRect(0, this.scale.height / 2, this.scale.width * percent, 50);
+
         })
 
         this.load.on('complete', () => {
-            // this.config.stopMessageLoop();
+            // this.loadingConfig.stopMessageLoop();
         })
 	}
 	create() {
@@ -54,9 +74,9 @@ export default class LoadingScene extends Phaser.Scene {
 		// since PlayerConfig is being passed the context of this scene (via inheritance),
 		// it is able to use scene.create on a specific scene. This will modify the 'this' object such that
 		// our this.player.anims still has access to all the animations created in a different file. (see utils folder)
-		this.config.createAnimations('loadingdino');
-		// config.startMessageLoop();
-		// config.stopMessageLoop() works | can stop the loop whenever we want
+		this.loadingConfig.createAnimations('loadingdino');
+		// loadingConfig.startMessageLoop();
+		// loadingConfig.stopMessageLoop() works | can stop the loop whenever we want
 	}
 	update() {
 		this.player.anims.play('idle', true);
