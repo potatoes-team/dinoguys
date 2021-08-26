0acf8456d6a6950f1badac4f8837113bacf48af7
Jaehoon Jung <jaehoon.jungg@gmail.com>
2021-08-26 15:26:13 -0400

    finished stage selection

--- src/scenes/StageSelection.js
+++ src/scenes/StageSelection.js
@@ -1,10 +1,4 @@
 import 'phaser';
-import StageDungeon from './StageDungeon';
-import StageForest from './StageForest';
-import StageSnow from './StageSnow';
-import config from '../config/config'
-import StageScene from './StageScene';
-
 
 export default class StageSelection extends Phaser.Scene {
   constructor () {
@@ -15,7 +9,6 @@ export default class StageSelection extends Phaser.Scene {
     const height = this.scale.height;
     const width = this.scale.width;
     const stageNames = ["StageForest", "StageDungeon", "StageSnow"]
-    // const stage = [StageForest, StageDungeon, StageSnow];
     for (let i = 0; i < 3; ++i) {
       const displayedNames = this.add.text(width * 0.3, height * (i / 8), stageNames[i], {
         fontSize: '30px',
@@ -24,7 +17,7 @@ export default class StageSelection extends Phaser.Scene {
       displayedNames.setInteractive();
       displayedNames.on('pointerup', () => {
         this.scene.stop('StageSelection');
-        this.scene.start(stageNames[i] /* , stage[i] */)
+        this.scene.start(stageNames[i], { isMultiplayer: false})
       })
     }
   }
