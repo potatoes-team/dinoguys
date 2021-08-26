e31055f82e8f04b5d090d4b5f1b3df39990c4df9
Jaehoon Jung <jaehoon.jungg@gmail.com>
2021-08-26 17:20:07 -0400

    styled the stage name

--- src/scenes/StageSelection.js
+++ src/scenes/StageSelection.js
@@ -5,15 +5,24 @@ export default class StageSelection extends Phaser.Scene {
     super('StageSelection');
   }
 
+  preload() {
+    this.load.image('castle-name', "assets/StageFont/Castle.png")
+    this.load.image('forest-name', "assets/StageFont/Forest.png")
+    this.load.image('snow-name', "assets/StageFont/Snow.png")
+  }
+
   create() {
     const height = this.scale.height;
     const width = this.scale.width;
     const stageNames = ["StageForest", "StageDungeon", "StageSnow"]
+    const stageImages = ['forest-name', 'castle-name', 'snow-name']
+
     for (let i = 0; i < 3; ++i) {
-      const displayedNames = this.add.text(width * 0.3, height * (i / 8), stageNames[i], {
+      const displayedNames = this.add.image(width * ((i+1) / 4), height * 0.2, stageImages[i]/* , {
         fontSize: '30px',
         fill: '#fff'
-      });
+      } */)/* .setOrigin(1, -5) */;
+      
       displayedNames.setInteractive();
       displayedNames.on('pointerup', () => {
         this.scene.stop('StageSelection');
