aa19594298de9afd863f69a52ba661a5ff66827f
super-rogatory <chukwudi10@gmail.com>
2021-08-26 21:52:17 -0700

    save. slow computer - might crash

--- src/scenes/UsernameScene.js
+++ src/scenes/UsernameScene.js
@@ -3,26 +3,34 @@ export default class UsernameScene extends Phaser.Scene {
 		super('UsernameScene');
 		this.state = {};
 	}
-	preload() {
-		this.state.textObject = this.add
-			.text(this.scale.width / 2, this.scale.height / 2, 'Username Scene', {
-				fontSize: '32px',
+	preload() {}
+	create() {
+		// Load text - > What's your dino name?
+		const test = this.add
+			.text(this.scale.width / 2, this.scale.height / 2 - 100, "What's your dino name?", {
+				fontFamily: 'Pricedown',
+				fontSize: '20px',
 			})
 			.setOrigin(0.5, 0.5);
-	}
-	create() {
-		const usernameForm = this.cache.html.get('usernameform');
-		const usernameBox = this.add.graphics();
-		usernameBox.strokeRect(425, 200, 275, 100);
-		usernameBox.fillRect(425, 200, 275, 100);
-		const inputElement = (this.add.scene.inputElement = this.add
-			.dom(562.5, 250)
-			.createFromCache('usernameform'));
-		inputElement.on('click', function (e) {
-			e.preventDefault();
-			if (e.target.name === 'enter-username') {
-				const input = inputElement.getChildByName('username-form');
-				console.log(input.value);
+		// Capture Dom Element from LoadingScene.js
+		const element = this.add.dom(400, 0).createFromCache('usernameform');
+		element.addListener('click', function (e) {
+			// if we clicked on the enter button
+			if (e.target.name === 'enter-username-button') {
+				const inputText = this.getChildByName('username-form');
+				// if user has entered information
+				if (inputText.value) {
+					this.removeListener('click');
+					this.setVisible(false);
+					test.setText(inputText.value);
+				} else {
+					this.scene.tweens.add({
+						targets: test,
+						duration: 200,
+						ease: 'Power3',
+						yoyo: true,
+					});
+				}
 			}
 		});
 	}
