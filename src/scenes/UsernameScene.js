84e0e7ec417f9cd539ec422a592d50156a73879a
super-rogatory <chukwudi10@gmail.com>
2021-08-27 17:19:46 -0700

    Feature: Enter your dino name! Is typed out

--- src/scenes/UsernameScene.js
+++ src/scenes/UsernameScene.js
@@ -4,9 +4,9 @@ export default class UsernameScene extends Phaser.Scene {
 	constructor() {
 		super('UsernameScene');
 		this.state = {
-			COLOR_PRIMARY: 0x4e342e,
-			COLOR_LIGHT: 0x7b5e57,
-			COLOR_DARK: 0x260e04,
+			COLOR_PRIMARY: 0x4e342e, // controls the background color of the textbox
+			COLOR_LIGHT: 0x7d7371, // controls the outlining of the textbox
+			// COLOR_DARK: 0x0d1b1e, // can use to color icons
 			titleText: 'Enter your dino name!',
 		};
 	}
@@ -15,15 +15,14 @@ export default class UsernameScene extends Phaser.Scene {
 		// createTextBox is a helper method that exists in the UsernameSceneConfig
 		const usernameConfig = new RexConfig(this);
 		usernameConfig
-			.createTextBox(this.scale.width / 2, this.scale.height / 2, {
-				wrapWidth: 500,
-				fixedWidth: 500,
-				fixedHeight: 50,
+			.createNormalTextBox(this.scale.width / 2, this.scale.height / 2, {
+				fixedWidth: 300,
+				fixedHeight: 30,
 				colorPrimary: this.state.COLOR_PRIMARY,
-				colorDark: this.state.COLOR_DARK,
+				// colorDark: this.state.COLOR_DARK,
 				colorLight: this.state.COLOR_LIGHT,
 			})
-			.start(this.state.titleText, 20);
+			.start(this.state.titleText, 30);
 	}
 	update() {}
 }
--- src/utils/RexUIConfig.js
+++ src/utils/RexUIConfig.js
@@ -2,23 +2,23 @@ export default class UsernameSceneConfig {
 	constructor(scene) {
 		this.scene = scene;
 	}
-	createTextBox(x, y, config) {
+	createNormalTextBox(x, y, config) {
+		// no wrapping, paging, or special features
 		// this method accepts an x, y, and config OBJECT with properties I grab
 		const { scene } = this;
-		const { wrapWidth, fixedWidth, fixedHeight, colorPrimary, colorLight } = config;
+		const { fixedWidth, fixedHeight, colorPrimary, colorLight } = config;
 		const textBox = scene.rexUI.add
 			.textBox({
 				x: x,
 				y: y,
 				background: scene.rexUI.add.roundRectangle(0, 0, 2, 2, 20, colorPrimary).setStrokeStyle(2, colorLight),
-				text: this.getText(wrapWidth, fixedWidth, fixedHeight),
+				text: this.getText(0, fixedWidth, fixedHeight),
 				orientation: 0,
 				space: {
 					left: 20,
 					right: 20,
 					top: 20,
 					bottom: 20,
-					text: 10,
 				},
 			})
 			.setOrigin(0.5, 0.5)
@@ -30,7 +30,7 @@ export default class UsernameSceneConfig {
 		const { scene } = this;
 		return scene.add
 			.text(0, 0, '', {
-				fontSize: '20px',
+				fontSize: '24px',
 				wordWrap: {
 					width: wrapWidth,
 				},
