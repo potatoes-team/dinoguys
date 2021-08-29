8fdba42a69586aa1ea0668261638c2ede5281590
super-rogatory <chukwudi10@gmail.com>
2021-08-29 13:45:56 -0700

    Feature: fully functional usernamescene, slight refactoring

--- src/scenes/MainMenuScene.js
+++ src/scenes/MainMenuScene.js
@@ -5,11 +5,14 @@ export default class MainMenuScene extends Phaser.Scene {
 
 	init(data) {
 		this.socket = data.socket;
+		this.username = data.name;
 	}
 
 	create() {
+		// chuck
+		console.log(this.username + ' is alive and well from the MainMenuScene');
+		
 		const { width, height } = this.scale;
-
 		this.add.text(width / 2, height / 4, 'Main Menu Scene', { fontSize: '32px' }).setOrigin(0.5, 0.5);
 
 		const singlePlayerBtn = this.add
--- src/scenes/UsernameScene.js
+++ src/scenes/UsernameScene.js
@@ -1,4 +1,4 @@
-import RexConfig from '../utils/RexUIConfig';
+import UsernameConfig from '../utils/UsernameSceneConfig';
 
 export default class UsernameScene extends Phaser.Scene {
 	constructor() {
@@ -11,7 +11,7 @@ export default class UsernameScene extends Phaser.Scene {
 	// 	this.socket = data.socket;
 	// }
 	create() {
-		const usernameConfig = new RexConfig(this);
+		const usernameConfig = new UsernameConfig(this);
 		// creates the title box with type effect
 		usernameConfig
 			.createTypingText(this.scale.width / 2, 200, {
@@ -21,9 +21,9 @@ export default class UsernameScene extends Phaser.Scene {
 			})
 			.start(this.state.titleText, 35); // (text, speed of typing).
 
-		// create textbox that can be edited
-		this.time.delayedCall(1500, () =>
-			usernameConfig.createTextBoxEditor(this.scale.width / 2, this.scale.height / 2, {
+		// create textbox that can be edited, this method is responsible for starting the next scene on a particular event.
+		this.time.delayedCall(1250, () =>
+			usernameConfig.createTextBoxEditorAndEvents(this.scale.width / 2, this.scale.height / 2 - 50, {
 				textColor: 0xffffff,
 				fontSize: '24px',
 				fixedWidth: 300,
