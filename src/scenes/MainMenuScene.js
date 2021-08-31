5c7346fb1863be79783421632ecaa783ca8e2c89
super-rogatory <chukwudi10@gmail.com>
2021-08-30 22:46:15 -0700

    Single player character pops up

--- src/scenes/MainMenuScene.js
+++ src/scenes/MainMenuScene.js
@@ -1,4 +1,5 @@
 import MainMenuSceneConfig from '../utils/MainMenuSceneConfig';
+import PlayerConfig from '../utils/PlayerConfig';
 
 export default class MainMenuScene extends Phaser.Scene {
 	constructor() {
@@ -13,12 +14,39 @@ export default class MainMenuScene extends Phaser.Scene {
 		this.load.image('title', 'assets/backgrounds/dinoguystitle.png');
 		this.load.image('main-menu-background', 'assets/backgrounds/bluebackground.jpg');
 		this.load.image('main-menu-crown', 'assets/sprites/crown.png');
+		this.load.spritesheet('dino', 'assets/spriteSheets/dino-blue.png', {
+			frameWidth: 15,
+			frameHeight: 18,
+			spacing: 9,
+		});
+		this.load.spritesheet('dino_red', 'assets/spriteSheets/dino-red.png', {
+			frameWidth: 15,
+			frameHeight: 18,
+			spacing: 9,
+		});
+		this.load.spritesheet('dino_yellow', 'assets/spriteSheets/dino-yellow.png', {
+			frameWidth: 15,
+			frameHeight: 18,
+			spacing: 9,
+		});
+		this.load.spritesheet('dino_green', 'assets/spriteSheets/dino-green.png', {
+			frameWidth: 15,
+			frameHeight: 18,
+			spacing: 9,
+		});
 	}
 	create() {
+		const playerConfig = new PlayerConfig(this);
 		const rexUIConfig = new MainMenuSceneConfig(this);
 		const { width, height } = this.scale;
-
 		// console.log(this.socket.id);
+
+		// creates animations on this scene.
+		playerConfig.createDinoAnimations('dino');
+		playerConfig.createDinoAnimations('dino_red');
+		playerConfig.createDinoAnimations('dino_yellow');
+		playerConfig.createDinoAnimations('dino_green');
+
 		// setting the blue background
 		this.add.image(0, 0, 'main-menu-background').setOrigin(0);
 
@@ -37,12 +65,15 @@ export default class MainMenuScene extends Phaser.Scene {
 			isBackground: true,
 		});
 
+		rexUIConfig.startSinglePlayerCharLoop();
+
 		const singlePlayerBtn = this.add
-			.text(width / 3, (height / 4) * 2, 'Single-Player', { fontSize: '24px' })
+			.text(width / 3, (height / 4) * 2, 'Single-Player', { fontSize: '24px', color: '#000' })
 			.setOrigin(0.5, 0.5);
 		const multiplayerBtn = this.add
 			.text((width / 3) * 2, (height / 4) * 2, 'Multiplayer', {
 				fontSize: '24px',
+				color: '#000',
 			})
 			.setOrigin(0.5, 0.5);
 
--- src/utils/MainMenuSceneConfig.js
+++ src/utils/MainMenuSceneConfig.js
@@ -3,7 +3,10 @@ import RexUIConfig from './RexUIConfig';
 export default class MainMenuSceneConfig extends RexUIConfig {
 	constructor(scene) {
 		super(scene);
-		this.scene = scene;
+		this.state = {
+			singlePlayerCharLoop: undefined,
+			currentSprite: undefined,
+		};
 	}
 	createTextLabel(text, x, y, config) {
 		const { scene } = this;
@@ -27,5 +30,50 @@ export default class MainMenuSceneConfig extends RexUIConfig {
 			.setOrigin(0.5)
 			.layout();
 		return textBox;
+	}
+
+	startSinglePlayerCharLoop() {
+		const { scene, addRandomSprite } = this;
+
+		this.state.singlePlayerCharLoop = scene.time.addEvent({
+			callback: addRandomSprite,
+			delay: 5000,
+			callbackScope: this,
+			loop: true,
+		});
+	}
+
+	stopSinglePlayerCharLoop() {
+		this.state.singlePlayerCharLoop.destroy();
+	}
+
+	addRandomSprite() {
+		const { scene, randomKey } = this;
+		const key = randomKey();
+		if (!this.state.currentSprite) {
+			this.state.currentSprite = scene.add
+				.sprite(scene.scale.width * 0.33, scene.scale.height / 2 + 100, key)
+				.setScale(7)
+				.setInteractive();
+		} else {
+			this.state.currentSprite.destroy();
+			this.state.currentSprite = scene.add
+				.sprite(scene.scale.width * 0.33, scene.scale.height / 2 + 100, key)
+				.setScale(7)
+				.setInteractive();
+		}
+		this.state.currentSprite.play(`idle_${key}`, true);
+		this.state.currentSprite.on('pointerover', () => {
+			this.state.currentSprite.play(`run_${key}`, true);
+		});
+		this.state.currentSprite.on('pointerout', () => {
+			this.state.currentSprite.play(`idle_${key}`, true);
+		});
+	}
+
+	randomKey() {
+		const keys = ['dino', 'dino_red', 'dino_yellow', 'dino_green'];
+		const randomIndex = Math.floor(Math.random() * keys.length);
+		return keys[randomIndex];
 	}
 }
